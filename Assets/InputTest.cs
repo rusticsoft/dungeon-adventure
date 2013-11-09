eb606873e66d02c664fe67dc8855768730cb19e7
Nicholas Scheurich <nick@scheurich.me>
2013-11-09 14:42:20 -0600

    Portstuffis

---
+++ Assets/InputTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputTest : MonoBehaviour {
+	public GameObject portcullis;
+	public float speed;
+	
+	// Use this for initialization
+	void Start () {
+		print(portcullis.transform.position.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPosition = portcullis.transform.position;
+		
+		if (Input.GetKey(KeyCode.E))
+		{	
+			newPosition.y = newPosition.y + speed;
+		}
+		else if (newPosition.y > 0)
+		{
+			newPosition.y = newPosition.y - speed;
+		}
+		
+		portcullis.transform.position = newPosition;
+	}
+}
---
+++ Assets/PhysicalProperties.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicalProperties : MonoBehaviour {
+	
+	public float weight;
+	public Vector3 forceOn;
+	private float speed = 0.05f;
+	public bool grounded;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		grounded = transform.position.y <= 0.0f;
+		
+		Vector3 newPosition = transform.position;
+		
+		if (forceOn.y > weight) {
+	    	newPosition.y = newPosition.y + speed;
+		} else if (!grounded) {
+			newPosition.y = newPosition.y - speed;
+		}
+				
+		transform.position = newPosition;
+	}
+}
